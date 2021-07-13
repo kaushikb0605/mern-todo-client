017c8252a500f34efe4e824f14f76356aca30c1f
Kaushik Barua <kbarua21@gmail.com>
2021-07-14 00:06:33 +0530

    removed extra /

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import {useState,useEffect} from 'react';
 
-const API_BASE="https://mern-todolistbykb.herokuapp.com/";
+const API_BASE="https://mern-todolistbykb.herokuapp.com";
 
 function App() {
 
